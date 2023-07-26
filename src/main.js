6d729948c169e93204e3609225fdd3ef5c191940
Grecia Fragoso <naomistereo96@gmail.com>
2023-07-26 13:33:25 -0500

    commit para poder pullear

--- src/main.js
+++ src/main.js
@@ -54,5 +54,5 @@ for (let i=0; i < data.cards.length; i++) {
   document.querySelectorAll('[data-testid = "value"]')[i].textContent = "Value: " + data.cards[i].value
   document.querySelectorAll('[data-testid = "meaning-up"]')[i].textContent = "Meaning up: " + data.cards[i].meaning_up
   document.querySelectorAll('[data-testid = "meaning-rev"]')[i].textContent = "Meaning rev: " + data.cards[i].meaning_rev
-  document.querySelectorAll('[data-testid = "description"]')[i].textContent = "Description: " + data.cards[i].desc
+  //document.querySelectorAll('[data-testid = "description"]')[i].textContent = "Description: " + data.cards[i].desc
 }
