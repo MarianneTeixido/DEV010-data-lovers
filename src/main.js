08542924041e66d8693b49ec81cb66a89bf8a478
MarianneTeixido <ztp.txdo@gmail.com>
2023-07-26 12:13:30 -0600

    img adjust & css modifications

--- src/main.js
+++ src/main.js
@@ -14,12 +14,12 @@ document.getElementById("btn2").addEventListener("click", function(nav){
 
 let cardsHTML = '';
 data.cards.map(() => {
-  cardsHTML += '<div class="flip-card">\
-  <div class="flip-card-inner">\
-    <div class="flip-card-front">\
-      <img data-testid="img" ></img>\
-    </div>\
-    <div class="flip-card-back">\
+  cardsHTML += '<figure class="flip-card">\
+  <figure class="flip-card-inner">\
+    <figure class="flip-card-front">\
+      <img data-testid="img" width="100%;" height="100%" ></img>\
+    </figure>\
+    <figure class="flip-card-back">\
       <ul>\
         <li data-testid="type">Type: </li>\
         <li data-testid="short-name">Short name: </li>\
@@ -29,9 +29,9 @@ data.cards.map(() => {
         <li data-testid="meaning-rev">Meaning rev: </li>\
         <li data-testid="description">Description: </li>\
       </ul>\
-    </div>\
-  </div>\
-</div>\
+    </figure>\
+  </figure>\
+</figure>\
 <br>\
 '
 })
@@ -45,5 +45,5 @@ for (let i=0; i < data.cards.length; i++) {
   document.querySelectorAll('[data-testid = "value"]')[i].textContent = "Value: " + data.cards[i].value
   document.querySelectorAll('[data-testid = "meaning-up"]')[i].textContent = "Meaning up: " + data.cards[i].meaning_up
   document.querySelectorAll('[data-testid = "meaning-rev"]')[i].textContent = "Meaning rev: " + data.cards[i].meaning_rev
-  document.querySelectorAll('[data-testid = "description"]')[i].textContent = "Description: " + data.cards[i].desc
+  //document.querySelectorAll('[data-testid = "description"]')[i].textContent = "Description: " + data.cards[i].desc
 }
