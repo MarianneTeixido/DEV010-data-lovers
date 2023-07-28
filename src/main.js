db9485b7ed80c1b569ea1442c88e6e761ca00aa9
Grecia Fragoso <naomistereo96@gmail.com>
2023-07-27 20:44:41 -0500

    flex container for cards, glowing button main page

--- src/main.js
+++ src/main.js
@@ -177,22 +177,28 @@ function filtrar() {
 const sortName = document.getElementById("sort");
 sortName.onchange = function () {
   console.log(data.cards)
-if(sortName.checked){
-const sorted = data.cards.sort(function (a, b) {
-    if (a.name > b.name) {
-      return 1;
-    }
-    if (a.name < b.name) {
-      return -1;
-    }
-    // a must be equal to b
-    return 0;
-  });
-  for (let i=0; i < data.cards.length; i++) {
-    document.querySelectorAll('[data-testid = "img"]')[i].src = sorted[i].img
+  if(sortName.checked){
+    const sorted = data.cards.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+    for (let i=0; i < data.cards.length; i++) {
+      document.querySelectorAll('[data-testid = "img"]')[i].src = sorted[i].img //sólo estaba esta línea
+      document.querySelectorAll('[data-testid = "type"]')[i].textContent = "Type: " + sorted[i].type
+      document.querySelectorAll('[data-testid = "short-name"]')[i].textContent = "Short name: " + sorted[i].name_short
+      document.querySelectorAll('[data-testid = "name"]')[i].textContent = "Name: " + sorted[i].name
+      document.querySelectorAll('[data-testid = "value"]')[i].textContent = "Value: " + sorted[i].value
+      document.querySelectorAll('[data-testid = "meaning-up"]')[i].textContent = "Meaning up: " + sorted[i].meaning_up
+      document.querySelectorAll('[data-testid = "meaning-rev"]')[i].textContent = "Meaning rev: " + sorted[i].meaning_rev
 
     } 
-   console.log(data.cards);
+    console.log(data.cards);
   }
   
   if(!sortName.checked){
@@ -208,8 +214,14 @@ const sorted = data.cards.sort(function (a, b) {
       return 0;
     });
     for (let i=0; i < data.cards.length; i++) {
-      document.querySelectorAll('[data-testid = "img"]')[i].src = reversed[i].img
+      document.querySelectorAll('[data-testid = "img"]')[i].src = reversed[i].img //sólo estaba esta línea
+      document.querySelectorAll('[data-testid = "type"]')[i].textContent = "Type: " + reversed[i].type
+      document.querySelectorAll('[data-testid = "short-name"]')[i].textContent = "Short name: " + reversed[i].name_short
+      document.querySelectorAll('[data-testid = "name"]')[i].textContent = "Name: " + reversed[i].name
+      document.querySelectorAll('[data-testid = "value"]')[i].textContent = "Value: " + reversed[i].value
+      document.querySelectorAll('[data-testid = "meaning-up"]')[i].textContent = "Meaning up: " + reversed[i].meaning_up
+      document.querySelectorAll('[data-testid = "meaning-rev"]')[i].textContent = "Meaning rev: " + reversed[i].meaning_rev
   
-      } 
+    } 
   }
 }
