8d7316922edb7fd7796176757e2aca29bd0bc054
Grecia Fragoso <naomistereo96@gmail.com>
2023-08-02 09:08:08 -0500

    little css changes

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
 // estas funciones son de ejemplo
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+/*export const future = {
+  
+  getMyLuck: () => {
+    const userName = document.getElementById('user-name');
+    console.log(userName)
+    const userBirthday = document.getElementById('birthday');
+    console.log(userBirthday);
+  }
+
+};*/
