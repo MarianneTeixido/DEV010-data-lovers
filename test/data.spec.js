f5fe1ab5394678193573d718697badd90d701db5
Grecia Fragoso <naomistereo96@gmail.com>
2023-08-08 10:19:38 -0500

    test trials and sintaxis changes at HTML

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,18 +1,70 @@
-import { example, anotherExample } from '../src/data.js';
+import { data, filterCards, allArcans, minors, majors, sortCards, cardsHTML, arrayMinors, arrayMajors, sortOrder, sorted, reversed } from '../src/data.js';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe(filterCards, () => {
+  it('is an object', () => {
+    expect(typeof filterCards).toBe('object');
   });
 });
 
 
-describe('anotherExample', () => {
+describe(allArcans, () => {
+  it('is a function', () => {
+    expect(typeof allArcans).toBe('function');
+  });
+
+  it('returns `cardsHTML`', () => {
+    expect((allArcans(data))).toBe(cardsHTML);
+  });
+
+  it('returns `array`', () => {
+    expect(typeof cardsHTML).toBe('array');
+  });
+});
+
+describe(minors, () => {
+  it('is a function', () => {
+    expect(typeof minors).toBe('function');
+  });
+
+  it('returns `array`', () => {
+    expect((minors(data))).toBe(arrayMinors);
+  });
+
+  it('returns `array`', () => {
+    expect(typeof arrayMinors).toBe('array');
+  });  
+});
+
+describe(majors, () => {
+  it('is a function', () => {
+    expect(typeof majors).toBe('function');
+  });
+
+  it('returns `array`', () => {
+    expect(majors(data)).toBe(arrayMajors);
+  });
+
+  it('returns an array', () => {
+    expect(typeof cardsHTML).toBe('array');
+  });
+});
+
+describe(sortCards, () => {
+  it('is a function', () => {
+    expect(typeof sortCards).toBe('function');
+  });
+
+  it('returns `array`', () => {
+    expect(sortCards(sortOrder)).toBe(sorted);
+  });
+
+  it('returns `array`', () => {
+    expect(sortCards(!sortOrder)).toBe(reversed);
+  });
+});
+
+/*describe('anotherExample', () => {
   it('is a function', () => {
     expect(typeof anotherExample).toBe('function');
   });
@@ -20,4 +72,4 @@ describe('anotherExample', () => {
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
-});
+});*/
